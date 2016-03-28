8bef7c3a8634a273f7333045fb5c1808bb3feb7b
Oli Evans <oli@zilla.org.uk>
2016-03-28 18:19:45 +0100

    Refactor to common layout template

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 var start = Date.now()
 var db = require('./db')
 var mailer = require('./emails/mailer.js')(db)
+var config = require('config')
 var express = require('express')
 var bodyParser = require('body-parser')
 
 var app = express()
 app.enable('trust proxy')
 app.disable('x-powered-by')
+app.use(express.static('dist'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
 var routes = [
   require('./pages/routes/server.js'),
   require('./pages/inbox/server.js'),
   require('./pages/home/server.js')
-]
-
-routes.forEach(function (route) {
+].forEach(function (route) {
   route(app, db)
 })
 
-app.use(express.static('dist'))
-
-app.listen(1337, '127.0.0.1', () => {
-  console.log('P O S T - started in %sms', Date.now() - start)
+app.listen(config.port, '127.0.0.1', () => {
+  console.log('P O S T ≈ http://localhost:%s ≈ started in %sms', config.port, Date.now() - start)
 })
